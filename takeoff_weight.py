7f2c4ef15778cc3a2060ba3c092c2f970f2861c6
Alex Ballentine <aeballentine@wpi.edu>
2023-11-07 18:41:31 -0500

    Added back in the rest of takeoff_weight.py

--- takeoff_weight.py
+++ takeoff_weight.py
@@ -22,7 +22,7 @@ SFC = 0.9  # 1/hr
 # subsonic cruise velocity
 V_c = 600  # mph
 # max lift to drag ratio
-AR_wetted = 1.4 # from text
+AR_wetted = 1.4  # from text
 K_LD = 14
 L2D_max = K_LD * np.sqrt(AR_wetted)
 # cruise lift to drag ratio
@@ -36,17 +36,17 @@ E = 1 / 2  # in hr
 R_super = 75  # in mi
 # supersonic specific fuel consumption
 SFC_super = 1.5  # (lbm/hr)/lbf - https://www.grc.nasa.gov/www/k-12/airplane/sfc.html
-                # Need to convert to 1/hr, getting answer in s^2/(hr*ft) - ask Prof
+# Need to convert to 1/hr, getting answer in s^2/(hr*ft) - ask Prof
 # supersonic cruise velocity
-cruise = 50000 #ft
-ceiling = 55000 #ft
-Temp_cruise = 390 #degrees R
-M_super = 1.25 # Supersonic Mach No.
+cruise = 50000  # ft
+ceiling = 55000  # ft
+Temp_cruise = 390  # degrees R
+M_super = 1.25  # Supersonic Mach No.
 gamma = 1.4
-gas_constant = 287 # J/kgK
-V_super = (M_super*np.sqrt(gamma*gas_constant*(Temp_cruise*0.556)))*3.28
-            # converted temp to K, calculated m/s, multiplied by 3.28 to get 
-L2D_super = 0.5 * L2D_max #double check formula
+gas_constant = 287  # J/kgK
+V_super = (M_super * np.sqrt(gamma * gas_constant * (Temp_cruise * 0.556))) * 3.28
+# converted temp to K, calculated m/s, multiplied by 3.28 to get
+L2D_super = 0.5 * L2D_max  # double check formula
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
 # fuel fraction:
@@ -58,3 +58,39 @@ frac_climb = 0.985
 frac_subsonic = math.exp(-R * SFC / (V_c * L2D_cruise))
 # supersonic cruise
 frac_supersonic = math.exp(-R_super * SFC_super / (V_super * L2D_super))
+# loiter
+frac_loiter = math.exp(-E * SFC / L2D_max)
+# landing
+frac_landing = 0.995
+fuel_fraction = (
+    1
+    - frac_taxi * frac_climb * frac_subsonic
+    # * frac_supersonic
+    * frac_loiter * frac_landing
+)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# crew and payload weight
+W_crew = 175 * 2
+W_payload = 0
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# empty weight ratio
+def weight_ratio(takeoff_weight):
+    A = 1.59
+    C = -0.10
+    empty_weight_ratio = A * takeoff_weight**C
+    return empty_weight_ratio
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+while error > 0.01:
+    W_new = (W_crew + W_payload) / (1 - fuel_fraction - weight_ratio(W_old))
+    error = np.abs(W_new - W_old)
+    weights_calc.append(W_new)
+    W_old = W_new
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# print the determined weight of the aircraft
+print("The empty weight is: ", np.round(weights_calc[-1], decimals=0), "lbs")
