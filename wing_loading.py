9b37230ff98393bfcf2e110cabf42f26e6ea97e2
emhealy <97701977+emhealy@users.noreply.github.com>
2023-11-28 14:47:19 -0500

    Running code but getting weird numbers, need to fix

--- wing_loading.py
+++ wing_loading.py
@@ -6,7 +6,7 @@ from scipy.optimize import fsolve
 # NOTE: multiple values labeled FIX that need to be checked/updated (based
 # on airfoil selection, etc. 
 
-def wing_loading(W0)
+def wing_loading(W0):
     #CRUISE
     #dynamic pressure at cruise
     V_cruise = 600 #mph
@@ -16,20 +16,21 @@ def wing_loading(W0)
     #wing dimensions
     b_w = 28  # wing span in ft
     S_w = 252  # wing area in ft^2
-    AR_w = bw**2 / S_w
+    AR_w = b_w**2 / S_w
     #lift to drag at cruise
     AR_wetted = 1.4  # from text
     K_LD = 14
     L2D_max = K_LD * np.sqrt(AR_wetted)
     L2D_cruise = 0.866 * L2D_max
     #weight at cruise
+    R = 1200 #subsonic range in mi
+    SFC = 0.9 #subsonic specific fuel consumption 1/hr
     W_cruise_initial = 0.97*0.985*W0
-    W_cruise_final = W_cruise_initial*math.exp(-Range*SFC/(V_cruise*L2D_cruise))
+    W_cruise_final = W_cruise_initial*math.exp(-R*SFC/(V_cruise*L2D_cruise))
     W_cruise = (W_cruise_final+W_cruise_initial)/2
     C_L = W_cruise * (1 + 2 / AR_w) / (q_c * S_w)
     C_D0 = 0.015
     wing_loading_cruise = q_c*math.sqrt((C_D0*math.pi*AR_w*e_0)/3)
-    print(wing_loading_cruise)
 
     #LOITER
     #dynamic pressure at loiter
@@ -38,7 +39,6 @@ def wing_loading(W0)
     density_loiter = 5.87*10**(-4) #density at 40kft FIX - FIND LOITER ALT
     q_l = 0.5*density_loiter*V_loiter**2
     wing_loading_loiter = q_l*math.sqrt(C_D0*math.pi*AR_w*e_0)
-    print(wing_loading_loiter)
 
     #LANDING
     #dynamic pressure at landing
@@ -48,17 +48,18 @@ def wing_loading(W0)
     #calculate wing loading at landing with C_Lmax
     C_Lmax = 2 #FIX for airfoil selection
     wing_loading_landing = C_Lmax*S_w*q_landing
-    print(wing_loading_landing)
     
     #TAKEOFF
-    #density_airport = density_SL #need to change? 
-    #TOP =
-    #sigma = density_airport / density_SL
-    #C_LTO = C_Lmax
-    #M_max = 1.25
-    #t_to_w_to = 0.488*M_max**0.728 #table 5.3
-    #wing_loading_TO = TOP*sigma*C_LTO*t_to_w_to
-    #print(wing_loading_TO)
-
-    return [wing_loading_cruise,wing_loading_loiter,wing_loading_landing,wing_loading_TO]
-
+    density_airport = density_SL #need to change?
+    S_g = 2500 #takeoff dist in ft based on baseline aircraft
+    TOP = 160 #based on S_g and Fig. 5.4
+    sigma = density_airport / density_SL
+    C_LTO = C_Lmax
+    M_max = 1.25
+    t_to_w_to = 0.488*M_max**0.728 #table 5.3
+    wing_loading_TO = TOP*sigma*C_LTO*t_to_w_to
+    #print('Wing loading at cruise: ' % wing_loading_cruise)
+    #print('Wing loading at loiter: ' % wing_loading_loiter)
+    #print('Wing loading at landing: ' % wing_loading_landing)
+    #print('Wing loading at takeoff: ' % wing_loading_TO) 
+    return [wing_loading_cruise, wing_loading_loiter, wing_loading_landing, wing_loading_TO]
